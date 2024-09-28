749f31021f41f58b303be17d024f6f6148d056c3
Wojtaz2345982 <blaszczykwojcicech54@gmail.com>
2024-09-28 21:41:31 +0200

    GetChats added and working

---
+++ SpaceY.RestApi/Contracts/Dtos/ChatSummaryDto.cs
@@ -0,0 +1,7 @@
+﻿namespace SpaceY.RestApi.Contracts.Dtos;
+
+public class ChatSummaryDto
+{
+    public Guid Id { get; set; }
+    public string? Title { get; set; }
+}
---
+++ SpaceY.RestApi/Features/Chats/GetChats.cs
@@ -0,0 +1,68 @@
+﻿using Carter;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SpaceY.RestApi.Contracts.Dtos;
+using SpaceY.RestApi.Database;
+using SpaceY.RestApi.Services;
+using SpaceY.RestApi.Shared;
+
+namespace SpaceY.RestApi.Features.Chats;
+
+public static class GetChats
+{
+    public class Query : IRequest<Result<List<ChatSummaryDto>>>  {  }
+
+    internal sealed class Handler : IRequestHandler<Query, Result<List<ChatSummaryDto>>>
+    {
+        private readonly IUserContext _userContext;
+        private readonly AppDbContext _dbContext;
+
+        public Handler(IUserContext userContext,
+            AppDbContext dbContext)
+        {
+            _userContext = userContext;
+            _dbContext = dbContext;
+        }
+
+        public async Task<Result<List<ChatSummaryDto>>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var userId = _userContext.GetCurrentUserId();
+
+            var dtos = await _dbContext.Chats
+                .Where(c => c.UserId == userId)
+                .OrderByDescending(c => c.CreatedAt)
+                .Select(c => new ChatSummaryDto
+                {
+                    Id = c.Id,
+                    Title = c.Title,
+                }).ToListAsync();
+
+            return Result.Success(dtos);
+        }
+    }
+}
+
+
+public class GetChatsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("chats", async (ISender sender) =>
+        {
+            var query = new GetChats.Query();
+
+            var result = await sender.Send(query);
+
+            if (result.IsFailure)
+            {
+                return Results.BadRequest(result.Error);
+            }
+
+            return Results.Ok(result.Value);
+        })
+        .RequireAuthorization()
+        .WithTags("Chats");
+
+    }
+}
